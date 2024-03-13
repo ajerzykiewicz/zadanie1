62bf58e5596201e869efe605de868e42417d1761
Andrzej Jerzykiewicz <83587@student.ue.poznan.pl>
2024-03-13 11:19:03 +0100

    dodanie trojkata

--- Exercise/exercise.py
+++ Exercise/exercise.py
@@ -7,3 +7,10 @@ bok_b = 12
 bok_c = 4
 pole_pr = bok_b * bok_c
 print("Pole prostokąta wynosi: " + str(pole_pr))
+
+#trojkat
+podstawa = 10
+wysokosc = 8
+pole_tr = podstawa * wysokosc / 2
+print("Pole trójkąta wynosi " + str(pole_tr))
+
